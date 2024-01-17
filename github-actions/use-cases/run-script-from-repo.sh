e10ebe4a7e7c23b2c16f9bd7e630fa9ffc788365
Jochen Metzger <t3sec.jm@googlemail.com>
2024-01-17 14:11:55 +0100

    Create run-script-from-repo.sh

---
+++ github-actions/use-cases/run-script-from-repo.sh
@@ -0,0 +1,44 @@
+# Run script from rep 
+
+## Step 1: Create script in repo 
+
+```
+# scripts/deploy.sh
+```
+
+```
+#!/bin/bash 
+
+echo "test this" 
+env
+echo $GITHUB_OUTPUT 
+echo "VORNAME=Hans" >> $GITHUB_OUTPUT
+```
+
+## Step 2: .github/workflows/blank.yml
+
+```
+name: CI
+
+# Controls when the workflow will run
+on:
+  # Triggers the workflow on push or pull request events but only for the "master" branch
+  push:
+    branches: [ "master" ]
+
+  # Allows you to run this workflow manually from the Actions tab
+  workflow_dispatch:
+  
+# A workflow run is made up of one or more jobs that can run sequentially or in parallel
+jobs:
+  run-playbooks:
+    runs-on: ubuntu-latest
+    steps: 
+      - uses: actions/checkout@v4
+      - name: run deploy 
+        shell: bash
+        run: |
+         cd scripts
+         chmod u+x ./deploy.sh
+         ./deploy.sh 
+```
